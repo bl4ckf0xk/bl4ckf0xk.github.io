9013ffb1476c92c25c4eb715dc157bfc523f6e05
bl4ckf0xk <kavinduksks@gmail.com>
2025-09-05 13:53:49 +0530

    Updates

---
+++ _next/static/cgtzFU8DmWROPp90eBnq8/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/37dd2a74fc8dfccb.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/37dd2a74fc8dfccb.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/efcdbde42ba386fc.js",
+  "static/chunks/turbopack-3c51c57c41b36413.js"
+])
